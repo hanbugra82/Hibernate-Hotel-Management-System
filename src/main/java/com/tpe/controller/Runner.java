a07bffe475a70c7e649691c392e276beb52f9cd4
hanbugra82 <hanbugra@yahoo.com>
2026-08-14 05:35:14 +0300

    hotem management hibernate done

--- src/main/java/com/tpe/controller/Runner.java
+++ src/main/java/com/tpe/controller/Runner.java
@@ -4,31 +4,31 @@ import com.tpe.config.HibernateUtils;
 import org.hibernate.Session;
 
 public class Runner {
-
-
     public static void main(String[] args) {
 
-        /*System.out.println("------Veritabani testi baslatiliyor");
+/*        System.out.println("------Veritabani testi baslatiliyor-------");
 
         try {
 
             Session session = HibernateUtils.getSessionFactory().openSession();
 
-            if(session != null && session.isOpen()) {
-                System.out.println(" Basarili: Veri tabanina baglandiniz ve session acildi");
+            if (session != null && session.isOpen()) {
+
+                System.out.println(" BASARILI: Veritabanina baglandiniz ve Session acildi");
+
                 HibernateUtils.closeSession(session);
                 HibernateUtils.shutdown();
-                System.out.println("----------Baglanti güvenli bir sekilde kapatildi----------");
+                System.out.println("----------Baglanti guvenli bir sekilde kapatildi-----------");
+
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.err.println("HATA: Baglanti kurulurken bir sorun olustu!");
             e.printStackTrace();
         }*/
 
-
+        //Uygulamayi baslat
+        HotelManagementApp.displayHotelManagementAppMenu();
 
     }
-
-
 }
